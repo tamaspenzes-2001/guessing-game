f8df97550571ebee35ab3882ca43eebc88f8e68a
tamaspenzes-2001 <penzestamas@proton.me>
2024-03-05 14:48:26 +0100

    add score system

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ class Game:
   def __init__(self, game_type, choices):
     self._guesses = 0
     self._correct_guesses = 0
+    self._score = 0
     self._game_type = game_type
     self._choices = choices
 
@@ -17,8 +18,15 @@ class Game:
     if (guess == outcome):
       print("\033[92;1mCongratulations! You nailed it!\033[0m")
       self._correct_guesses += 1
+      self._score += 20
+      print("\033[93;1mScore: " + str(self._score) + "\033[0m \033[92;1m(+20)\033[0m")
     else:
       print("\033[91;1mIncorrect tip. Maybe next time.\033[0m")
+      if (self._score > 0):
+        self._score -= 10
+        print("\033[93;1mScore: " + str(self._score) + "\033[0m \033[91;1m(-10)\033[0m")
+      else:
+        print("\033[93;1mScore: " + str(self._score))
 
   def get_stats(self):
     print("\033[93;1mCorrect tips in " + self._game_type + ": " + str(self._correct_guesses) + "/" + str(self._guesses) + "\033[0m")
